1d944286c56ae7dad3f935ac2bed73247cadf6fd
pongpak <pongpakpattawee.com>
2026-07-03 17:32:05 +0000

    fix message at messages

--- messages.php
+++ messages.php
@@ -1,12 +1,12 @@
 <?php
 echo '<div class="message-box">
-    <strong>[netiwit]:</strong> Hello Pongpak i'm netiwit
+    <strong>[netiwit]:</strong> Hello Pongpak im netiwit
     <br>
     <span style="font-size: 12px; color: #666;">-> STATUS: netiwit69319010010</span>
 </div>';
 
 echo '<div class="message-box">
-    <strong>[Praveen]:</strong> Hello Pongpak i'm Praveen
+    <strong>[Praveen]:</strong> Hello Pongpak im Praveen
     <br>
     <span style="font-size: 12px; color: #666;">-> STATUS: Praveen69319010009</span>
 </div>';
