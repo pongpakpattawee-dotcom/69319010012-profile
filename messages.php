2780e2b5a1d25b67194d92d0a2aef960bd325e42
sandwichkongutsa-sy <sandwichkongutsa@gmail.com>
2026-07-04 12:56:13 +0000

    fixmessagesname

--- messages.php
+++ messages.php
@@ -1,13 +1,13 @@
 <?php
 echo '<div class="message-box">
-    <strong>[netiwit]:</strong> Hello Pongpak im netiwit
-    <br>
-    <span style="font-size: 12px; color: #666;">-> STATUS: netiwit69319010010</span>
-</div>';
+        <strong>[netiwit]:</strong> Hello Pongpak im netiwit
+        <br>
+        <span style="font-size: 12px; color: #666;">-> STATUS: netiwit69319010010</span>
+      </div>';
 
 echo '<div class="message-box">
-    <strong>[Praveen]:</strong> Hello Pongpak im Praveen
-    <br>
-    <span style="font-size: 12px; color: #666;">-> STATUS: Praveen69319010009</span>
-</div>';
+        <strong>[Praveen]:</strong> Hello Pongpak im Praveen
+        <br>
+        <span style="font-size: 12px; color: #666;">-> STATUS: Praveen69319010009</span>
+      </div>';
 ?>
